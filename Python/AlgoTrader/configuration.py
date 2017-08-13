a07cf2efec889eac0229595bfbdb320f880226c2
mehrzadch <mehrzad.chegini@gmail.com>
2017-08-13 15:48:23 -0700

    Removal

--- Python/AlgoTrader/configuration.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun  3 23:01:22 2017
-
-@author: hassan
-"""
-historical_database="C:\\Users\hassan\Documents\Trading\Python codes\AlgoTrader\DataBase\HistoricalData"
